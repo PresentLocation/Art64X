5251e608bd93ad86f6cc4b7f22a8a35e5f7ae921
Eugene Ahn <10797905+PresentLocation@users.noreply.github.com>
2020-03-08 19:27:20 -0700

    Create phpBrainTeaser.php

---
+++ 200305/phpBrainTeaser.php
@@ -0,0 +1,17 @@
+<?php
+
+// ASSIGNMENTS
+$something  = "nothing";
+$nothing    = "everything";
+$everything = "something";
+
+// COMBINATION
+$anything   = $nothing . $everything . $something;
+
+// CONCATENATION, USING THE CONCATENATION ASSIGNMENT OPERATOR (".=")
+$anything   .= $anything;
+
+// CHALLENGE: WHAT WILL THE SCRIPT PRODUCE IF WE PRINT VARIABLE $anything ???
+//echo $anything;
+
+?>
